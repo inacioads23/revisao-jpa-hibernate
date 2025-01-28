ab26b4c201a8ebec801b28138181e668ab25ae5c
inacioads23 <jinacio11@gmail.com>
2025-01-28 18:34:56 -0300

    Update files program.java

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -30,6 +30,12 @@ public class Programa {
 		Pessoa p = em.find(Pessoa.class, 2);
 		System.out.println(p);
 		
+		//removendo registro do DB
+		Pessoa p1 = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p1);
+		em.getTransaction().commit();
+				
 		System.out.println("Pronto!");
 		em.close();
 		emf.close();
