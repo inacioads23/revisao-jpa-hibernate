f2d4bf779843ce7b8c5b1f3a9ef256fdfe6f578d
inacioads23 <jinacio11@gmail.com>
2025-01-28 18:28:41 -0300

    Update files pom.xml and program.java

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -10,17 +10,25 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
+		/*
 		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
 		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
 		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		*/
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
+		/*
+		em.getTransaction().begin(); // início da transação com o DB
 		em.persist(p1);
 		em.persist(p2);
 		em.persist(p3);
-		em.getTransaction().commit();
+		em.getTransaction().commit(); // confirmar as transação feitas no DB
+		*/
+		
+		//fazendo uma busca
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
 		
 		System.out.println("Pronto!");
 		em.close();
